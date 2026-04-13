2f0ff37af1b75869da15407477c5d41e45326ccf
ta05112k7-star <ta05112k7@gmail.com>
2026-04-13 13:13:05 +0700

    Add files via upload

---
+++ OOP/baitap5.1.py
@@ -0,0 +1,10 @@
+import time
+current_time = time.time()
+days = int(current_time // (24 * 3600))
+remaining_seconds = current_time % (24 * 3600)
+hours = int(remaining_seconds // 3600)
+remaining_seconds = remaining_seconds % 3600
+minutes = int(remaining_seconds // 60)
+seconds = int(remaining_seconds % 60)
+print("Days since epoch:", days)
+print("Current time:", hours, ":", minutes, ":", seconds)
